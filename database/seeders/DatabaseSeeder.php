3d0c116db5177adcdc3f2ecfc77e0f0d25e12c8d
Lazar Milosavljevic <milosavljeviclazar56@gmail.com>
2025-12-17 17:30:45 +0100

    Dopunjen seeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -9,6 +9,9 @@ use App\Models\Transakcija;
 use App\Models\Transfer;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+// use Illuminate\Support\Facades\Schema;
+
 
 class DatabaseSeeder extends Seeder
 {
@@ -19,11 +22,23 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
+        //ovo sluzi da obriseo stare podatke prilikom kreiranja novih
+        DB::statement('SET FOREIGN_KEY_CHECKS=0;'); //ova linija je da se iskljuci provera FK
+        Transfer::truncate();
+        Transakcija::truncate();
+        Novcanik::truncate();
+        Kategorija::truncate();
+        User::truncate();
+
+
         User::factory(10)->create();
         Kategorija::factory(30)->create();
         Novcanik::factory(20)->create();
         Transakcija::factory(100)->create();
         Transfer::factory(50)->create();
+
+
+        DB::statement('SET FOREIGN_KEY_CHECKS=1;');
 
 
         // User::factory()->create([
